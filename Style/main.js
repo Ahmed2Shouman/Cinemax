8b5b002b38a84be91e8fbf675e9b5b9b4fbce2e7
Ahmed Shouman <145878832+Ahmed2Shouman@users.noreply.github.com>
2025-05-31 15:42:25 +0300

    Updating the css of book page to make it responsive

--- Style/main.js
+++ Style/main.js
@@ -1,4 +1,5 @@
 function toggleSidebar() {
   const sidebar = document.querySelector('.sidebar');
+  const icon = document.querySelector('.sidebar-icon');
   sidebar.style.display = sidebar.style.display === 'flex' ? 'none' : 'flex';
 }
