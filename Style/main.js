347d3588e8c914adc40f8b8e62104665d4519dab
Ahmed Shouman <145878832+Ahmed2Shouman@users.noreply.github.com>
2025-06-01 17:58:38 +0300

    Update main.js

--- Style/main.js
+++ Style/main.js
@@ -1,5 +1,4 @@
 function toggleSidebar() {
   const sidebar = document.querySelector('.sidebar');
-  const icon = document.querySelector('.sidebar-icon');
   sidebar.style.display = sidebar.style.display === 'flex' ? 'none' : 'flex';
 }
